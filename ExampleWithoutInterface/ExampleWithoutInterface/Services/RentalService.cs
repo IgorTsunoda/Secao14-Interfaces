1758d1b1d4e8f5065fc18b00db7843db699daf6c
Igor Tsunoda <igortaikitsunoda@gmail.com>
2024-03-04 22:22:01 -0300

    Examplewithout interface part 1.

---
+++ ExampleWithoutInterface/ExampleWithoutInterface/Services/RentalService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ExampleWithoutInterface;
+using ExampleWithoutInterface.Entities;
+
+namespace ExampleWithoutInterface.Services
+{
+    internal class RentalService
+    {
+        public double PricePerHour { get; private set; }
+        public double PricePerDay { get; private set; }
+
+        public RentalService(double pricePerHour, double pricePerDay)
+        {
+            PricePerHour = pricePerHour;
+            PricePerDay = pricePerDay;
+        }
+
+        public void ProcessInvoice(CarRental carRental)
+        {
+
+        }
+    }
+}
