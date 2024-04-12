c2fd97ea012fd69accf9f2b8ef359ab233f660d3
Igor Tsunoda <igortaikitsunoda@gmail.com>
2024-04-12 16:28:12 -0300

    solution without interfaces finished.

--- SolutionWithoutInterface/Program.cs
+++ SolutionWithoutInterface/Program.cs
@@ -1,4 +1,5 @@
 ﻿using SolutionWithoutInterface.Entities;
+using SolutionWithoutInterface.Service;
 using System.Globalization;
 
 namespace SolutionWithoutInterface
@@ -8,15 +9,25 @@ namespace SolutionWithoutInterface
         static void Main(string[] args)
         {
             Console.WriteLine("Enter rental data");
-            Console.WriteLine("Car model:");
+            Console.Write("Car model:");
             string model = Console.ReadLine();
-            Console.WriteLine("Pickup (dd/MM/yyyy hh:mm): ");
-            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy hh:mm", CultureInfo.InvariantCulture);
-            Console.WriteLine("Return (dd/MM/yyyy hh:mm): ");
-            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy hh:mm", CultureInfo.InvariantCulture);
+            Console.Write("Pickup (dd/MM/yyyy hh:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.Write("Return (dd/MM/yyyy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
 
             CarRental carRental = new CarRental(start, finish, new Vehicle(model));
 
+            Console.Write("Enter price per hour: ");
+            double hour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Enter price per day: ");
+            double day = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            RentalService rentalService = new RentalService(hour, day);
+
+            rentalService.ProcessInvoice(carRental);
+            Console.WriteLine("INVOICE:");
+            Console.WriteLine(carRental.Invoice);
         }
     }
 }
--- SolutionWithoutInterface/Service/RentalService.cs
+++ SolutionWithoutInterface/Service/RentalService.cs
@@ -11,6 +11,7 @@ namespace SolutionWithoutInterface.Service
     {
         public double PricePerHour { get; private set; }
         public double PricePerDay { get; private set; }
+        private BrazilTaxService _brazilTaxService = new BrazilTaxService();
 
         public RentalService(double pricePerHour, double pricePerDay)
         {
@@ -20,7 +21,13 @@ namespace SolutionWithoutInterface.Service
 
         public void ProcessInvoice(CarRental carRental)
         {
+            TimeSpan duration = carRental.Finish.Subtract(carRental.Start);
+            double basicPayment = 0.0;
+            basicPayment = duration.TotalHours > 12.0 ? Math.Ceiling(PricePerDay * duration.TotalDays) : 
+                Math.Ceiling(PricePerHour * duration.TotalHours);
 
+            double tax = _brazilTaxService.Tax(basicPayment);
+            carRental.Invoice = new Invoice(basicPayment, tax);
         }
     }
 }
