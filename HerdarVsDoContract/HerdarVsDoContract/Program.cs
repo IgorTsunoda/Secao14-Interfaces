c4302aad7ccf0196be87f9b7edfcb72ebf7dfaa1
Igor Tsunoda <igortaikitsunoda@gmail.com>
2024-04-16 23:22:57 -0300

    Fix HerdarVsDoContract - With Interface

--- HerdarVsDoContract/HerdarVsDoContract/Program.cs
+++ HerdarVsDoContract/HerdarVsDoContract/Program.cs
@@ -7,8 +7,8 @@ namespace HerdarVsDoContract
     {
         static void Main(string[] args)
         {
-            AbstractShape s1 = new Circle() {Radius = 2.0, Color = Color.White };
-            AbstractShape s2 = new Retangle() { Width = 3.5, Height = 4.2, Color = Color.Black };
+            IShape s1 = new Circle() {Radius = 2.0, Color = Color.White };
+            IShape s2 = new Retangle() { Width = 3.5, Height = 4.2, Color = Color.Black };
 
             Console.WriteLine(s1);
             Console.WriteLine(s2);
